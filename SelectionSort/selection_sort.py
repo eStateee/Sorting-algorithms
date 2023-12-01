c74162c1df9af95af0666597989ba5d3a8094ae9
Alex <sasha.kirillov.minsk@gmail.com>
2023-12-01 17:52:27 +0300

    selection sort

---
+++ SelectionSort/selection_sort.py
@@ -0,0 +1,20 @@
+# Selection sort in Python
+# time complexity O(n*n)
+# sorting by finding min_index
+def selectionSort(array):
+    size = len(arr)
+    for i in range(size):
+        min_index = i
+
+        for j in range(i + 1, size):
+            # select the minimum element in every iteration
+            if array[j] < array[min_index]:
+                min_index = j
+        # swapping the elements to sort the array
+        (array[i], array[min_index]) = (array[min_index], array[i])
+
+
+arr = [-2, 45, 0, 11, -9, 88, -97, -202, 747]
+selectionSort(arr)
+print('The array after sorting in Ascending Order by selection sort is:')
+print(arr)
